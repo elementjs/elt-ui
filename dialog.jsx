fe6c99ff3c50b2f2db398f83185bfc72b3640401
Christophe Eymard <christophe.eymard@sales-way.com>
2015-10-23 18:50:03 +0200

    more progress

---
+++ dialog.jsx
@@ -0,0 +1,35 @@
+
+import {elt} from 'elt/node';
+import {Component} from 'elt/controller';
+import {o} from 'elt/observable';
+import {click} from 'elt/touch';
+import {bind, cls} from 'elt/decorators';
+
+
+export class Dialog extends Component {
+
+  view(attrs, content) {
+
+    return <div class='eltm-dialog-overlay'>
+        {content}
+      </div>;
+
+  }
+
+}
+
+/**
+ * A function that returns a promise and that allows us to show a nice dialog.
+ */
+export function dialog(opts) {
+
+  // opts.toolbar
+  // opts.title
+  // opts.fullscreen -- if we want the dialog to take up the whole space.
+
+}
+
+
+export function modal(opts) {
+  
+}
--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ module.exports = {
   Button: require('./button').Button,
   Checkbox: require('./checkbox').Checkbox,
   Icon: require('./icon').Icon,
+  Input: require('./input').Input,
   Radio: require('./radio').Radio,
   Toolbar: require('./toolbar').Toolbar,
 };
