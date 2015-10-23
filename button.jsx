210c8f673c6cff0f9efde44537474dbac513ddcc
Christophe Eymard <christophe.eymard@sales-way.com>
2015-10-23 16:21:38 +0200

    button now wants a click attribute

--- button.jsx
+++ button.jsx
@@ -1,7 +1,7 @@
 
 import {Component} from 'elt/controller';
 import {o} from 'elt/observable';
-import {cls} from 'elt/decorators';
+import {cls, click} from 'elt/decorators';
 import {elt} from 'elt/node';
 
 import './button.styl';
@@ -10,12 +10,24 @@ export class Button extends Component {
 
   view(attrs, children) {
     // FIXME missing ripple.
-    let fn = attrs.click || function () {};
+    // let fn = attrs.click || function () {};
 
-    return <button class='eltm-button' disabled={attrs.disabled}>
-      <span class='eltm-button-content' $$={cls({disabled: attrs.disabled, raised: attrs.raised})} >{children}</span>
+    let data = this.data = o.all({
+      disabled: attrs.disabled,
+      raised: attrs.raised,
+      fn: attrs.click || function () {}
+    });
+
+    return <button class='eltm-button' disabled={data.disabled} $$={click(this.doClick.bind(this))}>
+      <span class='eltm-button-content' $$={cls({disabled: data.disabled, raised: data.raised})} >{children}</span>
     </button>;
     // class={o(attrs.class, attrs.disabled, (v, d) => `${v||''} ${d ? 'disabled' : ''} eltm-button-content`)}
+  }
+
+  doClick(event) {
+    if (!this.data.disabled.get()) {
+      this.data.fn.get().call(this, event);
+    }
   }
 
 }
