71b0a3f1f57ff977d6807167e1bc315f7b62056c
Christophe Eymard <christophe.eymard@sales-way.com>
2015-11-01 23:20:40 +0100

    working dialog without the Component

--- dialog.jsx
+++ dialog.jsx
@@ -2,12 +2,13 @@
 import {elt} from 'elt/node';
 import {Controller} from 'elt/controller';
 import {o} from 'elt/observable';
-import {click} from 'elt/touch';
-import {bind, cls} from 'elt/decorators';
+import {bind, cls, ctrl, click} from 'elt/decorators';
 
 import {Button} from './button';
 
 import './dialog.styl';
+
+var NOSCROLL_CLASS = 'eltm-dialog-noscroll';
 
 export class DialogCtrl extends Controller {
   constructor() {
@@ -19,98 +20,79 @@ export class DialogCtrl extends Controller {
   }
 
   resolve(value) {
-    this.remove();
+    this.node.remove();
     this._resolve(value);
   }
 
   reject(value) {
-    this.remove();
+    this.node.remove();
     this._reject(value);
   }
 
 }
 
-// export class Dialog extends Component {
-//
-//   view(attrs, content) {
-//
-//     return <div class='eltm-dialog-overlay' $$={click(this.close.bind(this))}>
-//         <div class='eltm-dialog-root'>
-//           {content}
-//         </div>
-//       </div>;
-//
-//   }
-//
-//   link() {
-//     // FIXME should be a special decorator to add enter and leave.
-//     this.node.element.classList.add('elm-enter');
-//     requestAnimationFrame(() => {
-//       this.node.element.classList.remove('elm-enter');
-//     });
-//
-//     this.node.on('mount', (event, root, before) => {
-//       this.root = root;
-//       root.classList.add('eltm-modal-showing');
-//     })
-//   }
-//
-//   remove() {
-//     this.node.remove();
-//     this.root.classList.remove('eltm-modal-showing');
-//   }
-//
-//
-//   close(event) {
-//     if (event.target === this.node.element) {
-//       // Cancel everything.
-//       this.reject();
-//     }
-//   }
-//
-// }
 
-export function Modal(attrs, children) {
+export var Overlay = (attrs, children) => <div class='eltm-dialog-overlay'>{children}</div>
+export var Title = (attrs, children) => <h3 class='eltm-dialog-title'>{children}</h3>
+export var Content = (attrs, children) => <div class='eltm-dialog-content'>{children}</div>
 
-  return <div class='eltm-dialog-overlay' $$={click(this.close.bind(this))}>
-      <div class='eltm-dialog-root'>
-        {attrs.title ? <h3 class='elt-dialog-title'>{attrs.title}</h3> : null}
-        <div class='eltm-dialog-content'>
-          {o(attrs.text, (v) => v.split(/\s*\n\s*/).map((e) => <p>{e}</p>))}
-        </div>
-        <div class='eltm-dialog-buttonbar'>
-          {attrs.disagree ? <Button click={() => this.resolve(false)}>{attrs.disagree}</Button> : null}
-          {attrs.agree ? <Button click={() => this.resolve(true)}>{attrs.agree}</Button> : null}
-        </div>
-      </div>
-    </div>;
-
-}
+// FIXME this node should watch the width of its children to be able
+// to switch to the vertical presentation for dialog buttons.
+export var Buttonbar = (attrs, children) => <div class='eltm-dialog-buttonbar'>{children}</div>
+export var Root = (attrs, children) => <div class='eltm-dialog-root'>{children}</div>
 
 /**
  * A function that returns a promise and that allows us to show a nice dialog.
  */
-export function dialog(opts) {
+export function dialog(opts, cbk) {
 
-  let title = <h3>{opts.title}</h3>;
-  let content = <Dialog.Content>{content}</Dialog.Content>;
-  let buttons = <Dialog.Buttonbar>{opts.buttons}</Dialog.Buttonbar>;
+  if (cbk === undefined) {
+    cbk = opts;
+    opts = {};
+  }
 
-  // opts.toolbar
-  // opts.title
-  // opts.fullscreen -- if we want the dialog to take up the whole space.
+  let dlg = new DialogCtrl;
+
+  let res = <Overlay $$={[ctrl(dlg), click((ev) => dlg.reject())]}>{cbk(dlg)}</Overlay>;
+
+  if (opts.disableScrolling !== false) {
+    let parent_elt = null;
+    res.on('mount', function (ev, parent) {
+      parent_elt = parent;
+      parent.classList.add(NOSCROLL_CLASS);
+    });
+
+    res.on('unmount', function (ev) {
+      parent_elt.classList.remove(NOSCROLL_CLASS);
+    });
+  }
+
+  res.mount(opts.parent || document.body);
+
+  return dlg.promise;
 
 }
 
 
-export function modal(title, text, agree, disagree) {
+/**
+ * A modal dialog.
+ * @param  {Object} opts Options
+ * @return {Promise}
+ */
+export function modal(opts) {
 
-  let m = <Modal title={title} text={text} agree={agree} disagree={disagree}/>;
-  m.mount(document.body);
-
-  // maybe a .ctrl when we have a component ?
-  let ctrl = m.getController(Modal);
-
-  return ctrl.promise;
+  return dialog(opts, (dlg) =>
+    <Root>
+      {opts.title ? <Title>{opts.title}</Title> : null}
+      <Content>
+        {/* Split the text at \n into distinct paragraphs. */}
+        {o(opts.text, (v) => v.split(/\s*\n\s*/).map((e) => <p>{e}</p>))}
+      </Content>
+      <Buttonbar>
+        {opts.disagree ? <Button click={() => dlg.resolve(false)}>{opts.disagree}</Button> : null}
+        {opts.agree ? <Button click={() => dlg.resolve(true)}>{opts.agree}</Button> : null}
+      </Buttonbar>
+    </Root>
+  );
 
 }
