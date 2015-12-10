731a29a524fff1f59d7f98d3d51673756d5e94c0
Christophe Eymard <christophe.eymard@sales-way.com>
2015-12-10 13:15:37 +0100

    Select does more things

--- select.jsx
+++ select.jsx
@@ -11,47 +11,31 @@ export function Select(attrs, children) {
 
 	let node = <label class='carbm-select-label'>
 		<select class='carbm-select' $$={bind(_select)}>
-			{options.transform((opts) => opts.map((o, i) => <option value={i}>{o.name}</option>))}
+			{options.transform((opts) => opts.map((o, i) => <option value={i}>{o.label||o.name||o}</option>))}
 		</select>
 	</label>
 
 	let mod = false;
+	let touched = () => { if (mod) return true; mod = true; requestAnimationFrame(() => { mod = false; }); };
+
+	node.observe(options, (opts) => {
+		if (touched()) return;
+
+		_select.set('' + opts.indexOf(model.get()));
+
+	});
 
 	node.observe(model, (v) => {
-		if (mod) return;
+		if (touched()) return;
 
-		mod = true;
-		console.log(options.get().indexOf(v));
 		_select.set(''+ options.get().indexOf(v));
-		requestAnimationFrame(() => { mod = false; })
 	});
 
 	node.observe(_select, (v) => {
-		if (mod) return;
-		mod = true;
+		if (touched()) return;
 
 		model.set(options.get()[v]);
-		requestAnimationFrame(() => { mod = false; })
 	});
-
-	// node.observe(o(_select, model, options, function (_select, model, options) {
-	// 	console.log(arguments);
-
-	// 	let idx = options.indexOf(model);
-	// 	if (idx > -1 && idx !== _select) {
-	// 		console.log('???');
-	// 		_select.set(idx);
-	// 	}
-	// 	// console.log(_select, model, options);
-	// }));
-
-	// node.observe(_select, (v) => {
-	// 	//
-	// });
-
-	// node.observe(model, (mod) => {
-
-	// });
 
 	return node;
 }
