dfddc892f52f84a759740f2b6ef068db314a29d9
Christophe Eymard <christophe.eymard@sales-way.com>
2016-02-18 15:56:49 +0100

    allow a change attr on Select

--- select.jsx
+++ select.jsx
@@ -10,9 +10,14 @@ export function Select(attrs, children) {
 	let labelfn = attrs.labelfn || (o => o.label || o.name || o);
 
 	let _select = o(-1);
+	let decorators = [bind(_select)];
+
+	if (attrs.change) {
+		decorators.push(atom => atom.listen('change', ev => attrs.change.call(atom, model.get(), ev)))
+	}
 
 	let atom = <label class='carbm-select-label'>
-		<select class='carbm-select' $$={bind(_select)}>
+		<select class='carbm-select' $$={decorators}>
 			{options.tf((opts) => {
 				return opts.map((o, i) => <option value={i} selected={model.get() === o ? true : undefined}>{labelfn(o)}</option>);
 			})}
