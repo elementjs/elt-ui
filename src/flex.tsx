7e208e6620e975fd1b1b416d77194e208cda6849
Christophe Eymard <christophe.eymard@sales-way.com>
2016-11-03 12:01:17 +0100

    better styling support for flex (still experimental though)

--- src/flex.tsx
+++ src/flex.tsx
@@ -29,32 +29,47 @@ function _(elt: HTMLElement, prop: string, value: string) {
 
 	var style: any = elt.style
 	style[prop] = value
-	style[`-webkit-${prop}`] = value
+	style[`webkit${prop[0].toUpperCase() + prop.slice(1)}`] = value
 }
 
-function _parse_attributes(node: HTMLElement, at: FlexAttributes) {
+function value(v: string): string {
+	switch (v) {
+		case 'around': return 'space-around'
+		case 'between': return 'space-between'
+		case 'start': return 'flex-start'
+		case 'end': return 'flex-end'
+		case 'no': return 'nowrap'
+	}
+	return v
+}
 
-	var el = node
+function _parse_attributes(el: HTMLElement, at: FlexAttributes) {
+
 	var cls = el.classList
 
 	if (at.wrap != null) cls.add('dm-flex-wrap')
 	if (at.direction != null) {
 		if (!at.reverse)
-			_(el, 'flex-direction', at.direction)
+			_(el, 'flexDirection', at.direction)
 		else
-			_(el, 'flex-direction', at.direction === 'column' ? 'column-reverse' : 'row-reverse')
-	} else if (at.reverse != null) _(el, 'flex-direction', 'row-reverse')
+			_(el, 'flexDirection', at.direction === 'column' ? 'column-reverse' : 'row-reverse')
+	} else if (at.reverse != null) _(el, 'flexDirection', 'row-reverse')
 
-	if (at.grow != null) _(el, 'flex-grow', at.grow)
-	if (at.basis != null) _(el, 'flex-basis', at.basis)
+	if (at.grow != null) _(el, 'flexGrow', at.grow)
+	if (at.basis != null) _(el, 'flexBasis', at.basis)
 
 	if (at.absoluteGrow != null) {
-		_(el, 'flex-grow', at.absoluteGrow)
-		_(el, 'flex-basis', '0')
+		_(el, 'flexGrow', at.absoluteGrow)
+		_(el, 'flexBasis', '0')
 	}
 
-	if (at.align != null) _(el, 'align-items', at.align)
-	if (at.justify != null) _(el, 'justify-content', at.justify)
+	if (at.align != null) {
+		_(el, 'alignItems', at.align)
+	}
+
+	if (at.justify != null) {
+		_(el, 'justifyContent', at.justify)
+	}
 
 }
 
