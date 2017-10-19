fe4b47275a480841a72761fb4bbab97a7bb0c2b9
Christophe Eymard <christophe.eymard@sales-way.com>
2017-10-19 18:00:11 +0200

    fixed the ink

--- ink.tsx
+++ ink.tsx
@@ -1,53 +1,7 @@
 
-import {e, click, Mixin} from 'elt'
+import {click, Mixin} from 'elt'
 
 import * as s from './styling'
-
-export namespace CSS {
-
-	export const rippleAnim = s.keyframes('ripple', {
-		'100%': { transform: `scale(4)` }
-	})
-
-	export const containerRippleAnim = s.keyframes('container-ripple', {
-		'50%': {opacity: 0.2},
-		'100%': {opacity: 0}
-	})
-
-	export const animate = s.style('em-ink-animate')
-
-	export const ink = s.style('ink', {
-			display: 'block',
-			position: 'absolute',
-			backgroundColor: s.colors.Primary,
-			opacity: 0.4,
-			borderRadius: '100%',
-			transform: 'scale(0)',
-			pointerEvents: 'none',
-			marginTop: '-25px',
-			marginLeft: '-25px',
-			width: '50px',
-			height: '50px',
-		},
-		s.and(animate, {animation: `${rippleAnim} 0.45s both ease-out`})
-	)
-
-	export const container = s.style('container', {
-			display: 'block',
-			width: '100%',
-			height: '100%',
-			top: 0,
-			left: 0,
-			overflow: 'hidden',
-			position: 'absolute',
-			pointerEvents: 'none',
-
-			backgroundColor: s.colors.Primary,
-		},
-		s.and(animate, {animation: `${containerRippleAnim} 0.45s both ease-out`})
-	)
-
-}
 
 
 export function inker(node: Node, event: MouseEvent) {
@@ -55,10 +9,12 @@ export function inker(node: Node, event: MouseEvent) {
 	var clientX = event.pageX
 	var clientY = event.pageY
 
-	const inker = e('div', {class: CSS.ink}) as HTMLDivElement
-	const ink_container = e('div', {class: CSS.container},
-		inker
-	) as HTMLDivElement
+	const inker = <div class={CSS.ink}/> as HTMLDivElement
+	const backdrop = <div class={CSS.backdrop}/> as HTMLDivElement
+	const ink_container = <div class={CSS.container}>
+		{backdrop}
+		{inker}
+	</div> as HTMLDivElement
 
 	node.appendChild(ink_container)
 
@@ -68,7 +24,7 @@ export function inker(node: Node, event: MouseEvent) {
 		inker.style.top = `${clientY - bb.top}px`
 		inker.style.left = `${clientX - bb.left}px`
 		inker.classList.add(CSS.animate)
-		ink_container.classList.add(CSS.animate)
+		backdrop.classList.add(CSS.animate)
 		setTimeout(() => ink_container.remove(), 1000)
 	})
 	// var bb = atom.element.getBounding
@@ -88,3 +44,60 @@ export function inkClickDelay(fn: (ev: MouseEvent) => void) {
 		setTimeout(() => fn(ev), 150)
 	})
 }
+
+
+export namespace CSS {
+
+		export const rippleAnim = s.keyframes('ripple', {
+			'0%': {backgroundColor: s.colors.Primary},
+			'100%': { transform: `scale(4)` }
+		})
+
+		export const backdropRipple = s.keyframes('container-ripple', {
+			'0%': {opacity: 0.5, backgroundColor: s.colors.Primary},
+			'50%': {opacity: 0.2},
+			'100%': {opacity: 0}
+		})
+
+		export const animate = s.style('em-ink-animate')
+
+		export const ink = s.style('ink', {
+				display: 'block',
+				position: 'absolute',
+				opacity: 0.4,
+				borderRadius: '100%',
+				transform: 'scale(0)',
+				pointerEvents: 'none',
+				marginTop: '-25px',
+				marginLeft: '-25px',
+				width: '50px',
+				height: '50px',
+			},
+			s.and(animate, {animation: `${rippleAnim} 0.45s ease-out`})
+		)
+
+		export const container = s.style('container', {
+				display: 'block',
+				width: '100%',
+				height: '100%',
+				top: 0,
+				left: 0,
+				overflow: 'hidden',
+				position: 'absolute',
+				pointerEvents: 'none',
+
+			},
+		)
+
+		export const backdrop = s.style('backdrop',
+			{
+				width: '100%',
+				height: '100%',
+				top: 0,
+				left: 0,
+				position: 'absolute',
+			},
+			s.and(animate, {animation: `${backdropRipple} 0.45s ease-out`})
+		)
+
+	}
