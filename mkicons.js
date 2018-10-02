c6881a80cde2f54e6e6ad76da936215bd0936ff9
Christophe Eymard <christophe.eymard@gmail.com>
2018-10-02 23:42:20 +0200

    removed local _svg

--- mkicons.js
+++ mkicons.js
@@ -1,27 +1,42 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
+const root = path.join(require.resolve('@fortawesome/fontawesome-free'), '../..')
 
-const SVG_DIR = path.join(__dirname, '_svg')
+// const SVG_DIR = path.join(__dirname, '_svg')
 const OUT_DIR = path.join(__dirname, 'src', 'icons')
 
-const files = fs.readdirSync(SVG_DIR).filter(s => s.match(/\.svg$/))
+/**
+ * ...
+ * @param {string} dir The directory to read
+ * @param {string} suffix The suffix to add to a file
+ */
+function get_files(dir, suffix = '') {
+  const files = fs.readdirSync(path.join(root, dir)).filter(s => s.match(/\.svg$/))
 
-for (var f of files) {
-  const dest = path.basename(f).replace('.svg', '.tsx')
-  const clsname = f.replace(/(?:^|-)([a-z])/g, (s, a) => a.toUpperCase())
-    .replace(/\.svg$/, '')
-  const source = fs.readFileSync(path.join(SVG_DIR, f), {encoding: 'utf-8'})
-    .replace(`xmlns="http://www.w3.org/2000/svg" `, `class={Fa.icon} `)
+  for (var f of files) {
+    var dest = path.basename(f).replace(/\.svg$/, m => suffix + '.tsx')
+    const clsname = f.replace(/(?:^|-)([a-z])/g, (s, a) => a.toUpperCase())
+      .replace(/\.svg$/, '')
+    const source = fs.readFileSync(path.join(root, dir, f), {encoding: 'utf-8'})
+      .replace(`xmlns="http://www.w3.org/2000/svg" `, `class={Fa.icon} `)
+      .replace(/<!--[^]*-->/m, '')
 
-  const src = `import { Attrs } from 'elt'
+    const src = `import { Attrs } from 'elt'
 import { Fa } from './index'
 
 export default function ${clsname}(a: Attrs) {
   return ${source}
 }
 `
-  // console.log(src)
-  fs.writeFileSync(path.join(OUT_DIR, dest), src, {encoding: 'utf-8'})
+    // console.log(src)
+    fs.writeFileSync(path.join(OUT_DIR, dest), src, {encoding: 'utf-8'})
+    // console.log(OUT_DIR, dest)
 
+  }
 }
+
+get_files('svgs/brands')
+get_files('svgs/regular', '-regular')
+get_files('svgs/solid')
+
