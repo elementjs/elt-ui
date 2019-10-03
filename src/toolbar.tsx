5fb5d4eead897caa9a8fd935208401daf1ef4f0e
Christophe Eymard <christophe.eymard@sales-way.com>
2019-10-03 16:46:52 +0200

    moving styles to outside definitions

--- src/toolbar.tsx
+++ src/toolbar.tsx
@@ -6,17 +6,18 @@ import { style, rule } from 'osun';
 
 export function Toolbar(_: Attrs, children: DocumentFragment): Element {
 
-  return <div style={{height: '3rem'}} class={[
-    S.contrast_on_tint,
-    S.text.bold,
-    S.flex.row.alignCenter.gap('16px'),
-    S.box.padding('16px').paddingHorizontal('16px'),
-    Toolbar.css.toolbar,
-  ]}>{children}</div>
+  return <div style={{height: '3rem'}} class={Toolbar.css.toolbar}>{children}</div>
 }
 
 export namespace Toolbar.css {
-  export const toolbar = style('toolbar')
+
+  export const toolbar = style('toolbar',
+    S.contrast_on_tint,
+    S.text.bold,
+    S.flex.row.alignCenter.gap(16),
+    S.box.padding(16).paddingHorizontal(16),
+  )
+
   rule `${toolbar} > h3`({
     fontSize: S.SIZES.big
   })
